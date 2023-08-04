38de96beaa6afde825d1b79f0a710b041357f9e4
sharon nzaya <nzayangeta4149@gmail.com>
2023-08-04 16:51:47 +0300

    working auth

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const authRoutes = require('./routes/authRoutes')
+const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
+const { requireAuth, checkUser } = require('./middleware/authMiddleware');
 
 const app = express();
 
 // middleware
 app.use(express.static('public'));
 app.use(express.json());
+app.use(cookieParser());
 
 // view engine
 app.set('view engine', 'ejs');
@@ -18,6 +21,29 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
   .catch((err) => console.log(err));
 
 // routes
+app.get('*', checkUser); //apply the checkUser middleware in all get routes
 app.get('/', (req, res) => res.render('home'));
-app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
 app.use(authRoutes);
+
+// cookies
+//* MaxAge is the time limit here the cookie will expire after 1 day
+//* secure means the cookie is only gonna be sent when we have a https connection
+//* httpOnly means we can't access the cookie in the javascript 
+// app.get('/set-cookies', (req,res) =>{
+  // res.setHeader('Set-Cookie', 'newUser=true'); //setting cookie without middleware
+//   res.cookie('newUser', false); //using the middleware cookie-parser
+  // res.cookie('isEmployee', true, {maxAge: 1000 * 60 * 60 *24, secure:true, httpOnly:true});
+//   res.cookie('isEmployee', true, {maxAge: 1000 * 60 * 60 *24, httpOnly:true});
+
+//   res.send('you got the cookies')
+
+// });
+
+// app.get('/read-cookies', (req,res) =>{
+//   const cookies = req.cookies;
+//   console.log(cookies.newUser); //to grab a specific cookie
+
+//   res.json(cookies);
+
+// });
--- models/User.js
+++ models/User.js
@@ -37,12 +37,25 @@ const userSchema = new mongoose.Schema({
 // });
 
 
-//pre hook
+//pre hook and hash passwords in db
 userSchema.pre('save', async function(next){
     const salt = await bcrypt.genSalt();
     this.password = await bcrypt.hash(this.password, salt);
     next();
 });
+
+//static method to login user
+userSchema.statics.login = async function(email, password){
+    const user = await this.findOne({email});
+    if(user){
+       const auth = await bcrypt.compare(password, user.password);
+       if(auth){
+        return user;
+       }
+       throw Error('Incorrect password');
+    }
+    throw Error('Incorrect email')
+}
 
 
 const User = mongoose.model('user', userSchema);
